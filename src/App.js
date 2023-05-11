40fd1f2469c2dc57a900c4ea6037f4e745609a8e
jorge navarro <jn200107@gmail.com>
2023-05-11 18:43:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
 
-function App() {
+const App=()=> {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      .then(response => response.json())
+      .then(data => setPokemonList(data.results));
+  }, []);
+  // a qui hacemos el llamado a la api y guardamos la data que nos trae en la setPokemonList
+
+  const getPokemonDetails = (pokemonUrl) => {
+    fetch(pokemonUrl)
+      .then(response => response.json())
+      .then(data => setSelectedPokemon(data));
+
+  };
+
+  const renderPokemonList = () => {
+    return (
+      <ul>
+        {pokemonList.map(pokemon => (
+          <li key={pokemon.name} onClick={() => {getPokemonDetails(pokemon.url)}}>
+            {pokemon.name}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+
+
+
+  const renderPokemonDetails = () => {
+    if (selectedPokemon) {
+      const { sprites, name, id, types, stats, abilities, color } = selectedPokemon;
+      return (
+        <div style={{backgroundColor:color}} >
+          <img src={sprites.front_default} alt={name} />
+          <h2>{name}</h2>
+          <p>Number: {id}</p>
+          <p>Types: {types.map(type => type.type.name).join(', ')}</p>
+          <p>Stats: {stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(', ')}</p>
+          <p>Abilities: {abilities.map(ability => ability.ability.name).join(', ')}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> La Pokédex</h1>
+      <div className="PokemonList">{renderPokemonList()}</div>
+      <div className="PokemonDetails">{renderPokemonDetails()}</div>
     </div>
+
+
   );
 }
 
 export default App;
+
+
+
